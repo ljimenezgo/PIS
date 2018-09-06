5f2a9824aa3f1928cfec90d1da83151ff09d5041
Luis Jimenez Gonzales <ljimnezgo@unsa.edu.pe>
2018-09-05 21:31:21 -0500

    Correcion Agregar Alumno

--- Controlador/alumno.controller.php
+++ Controlador/alumno.controller.php
@@ -73,9 +73,9 @@ class AlumnoController{
 		$hash = password_hash($_REQUEST['persona_cui'], PASSWORD_BCRYPT);
         //Captura de los datos del formulario (vista).
         $pvd->persona_id = $_REQUEST['persona_cui'];
-        $pvd->persona_nombres = $_REQUEST['persona_nombres'];
-        $pvd->persona_apellido1 = $_REQUEST['persona_apellido1'];
-        $pvd->persona_apellido2 = $_REQUEST['persona_apellido2'];
+        $pvd->persona_nombres = ''.$_REQUEST['persona_apellido1'].'/'.$_REQUEST['persona_apellido2'].", ".$_REQUEST['persona_nombres'];
+        $pvd->persona_apellido1 = "";
+        $pvd->persona_apellido2 = "";
 		$pvd->persona_tipo_id = $_REQUEST['persona_tipo_id'];
         $pvd->persona_cui = $_REQUEST['persona_cui'];
         $pvd->persona_direccion = $_REQUEST['persona_direccion'];
