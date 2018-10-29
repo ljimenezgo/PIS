3e25a478336536c3c6dd8bf6c63a9c8ec03bd757
Luis Jimenez Gonzales <ljimnezgo@unsa.edu.pe>
2018-10-29 08:08:20 -0500

    Cambios

--- Vista/Profesor/lista-alumnos.php
+++ Vista/Profesor/lista-alumnos.php
@@ -19,7 +19,7 @@
                 <div class="col-lg-12">
                     <div class="panel panel-default">
                         <div class="panel-heading">
-                            Docentes Registrados
+                            Alumnos registrados en tutoría para el docente
                         </div>
                         <!-- /.panel-heading -->
                         <div class="panel-body">
